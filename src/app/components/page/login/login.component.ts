f57ea48c15e863f229928afa4f3d53de749d641c
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-10-28 19:13:29 +0530

    hi

---
+++ src/app/components/page/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+
+  constructor(private router : Router)  { }
+
+
+  onSubmit(){
+
+    alert("Hello");
+    console.log("onSubmit");
+    this.router.navigate(['home']);
+  }
+
+}
